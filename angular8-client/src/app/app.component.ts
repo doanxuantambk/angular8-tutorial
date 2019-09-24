e388622098c09d3235fe93a1de0a48beddae4f09
Tam Doan <doanxuantam@gmail.com>
2019-09-24 18:06:31 +0700

    Update

--- angular8-client/src/app/app.component.ts
+++ angular8-client/src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-import * as $ from 'jquery';
+import {Component, OnInit} from '@angular/core';
+// import * as $ from 'jquery';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular8-client';
+export class AppComponent implements OnInit {
+  constructor() { }
+  title = "Xin chao";
+
+  ngOnInit() {
+    $(document).ready(function() {
+
+      $('.multi-item-carousel').carousel({
+        interval: false
+      });
+
+      $('.multi-item-carousel .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length>0) {
+          next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+          $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+      });
+
+    });
+  }
 }
